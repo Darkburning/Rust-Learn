aa4fd24e90d72abda1a49b20973d521d2ca2736c
Darkburning <1581690775@qq.com>
2023-09-04 14:02:00 +0800

    error handle

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,24 @@ use rand::Rng; // trait 类似其他语言的interface
 use std::cmp::Ordering; // enum
 use std::io; // not prelude
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("expected 1-100 but got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the  number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
@@ -19,14 +37,15 @@ fn main() {
 
         // 变量遮蔽（shadow）,用于类型转换
         // 忽略非数字的猜测
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        // 定义一个类型来承载错误
+        let guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess.value());
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"), // arm
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
