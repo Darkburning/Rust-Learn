b460a9556c6bd21173cde0aa01e2c386afd47bd1
kingki663 <1581690775@qq.com>
2023-09-17 12:18:21 +0800

    利用closure和iterator提高效率

--- mygrep/src/lib.rs
+++ mygrep/src/lib.rs
@@ -1,4 +1,3 @@
-use dotenv::dotenv;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
@@ -11,14 +10,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &Vec<String>) -> Result<Self, &'static str> {
-        dotenv().ok();
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Self, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did't get a file name "),
+        };
+
         Ok(Self {
-            query: args[1].clone(),
-            filename: args[2].clone(),
+            query,
+            filename,
             case_sensetive: std::env::var("CASE_SENSETIVE")
                 .is_ok_and(|v| v == "true" || v == "ok" || v == "1"),
         })
@@ -42,50 +48,51 @@ pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> HashMap<usize, &'a str> {
-    let mut results = HashMap::new();
-
-    for (line_num, line) in contents.lines().enumerate() {
-        if line.contains(query) {
-            results.insert(line_num + 1, line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .enumerate()
+        .filter(|(_line_num, line)| line.contains(query))
+        .map(|(line_num, line)| (line_num + 1, line))
+        .collect()
 }
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> HashMap<usize, &'a str> {
     let query = query.to_lowercase();
-    let mut results = HashMap::new();
 
-    for (line_num, line) in contents.lines().enumerate() {
-        if line.to_lowercase().contains(&query) {
-            results.insert(line_num + 1, line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .enumerate()
+        .filter(|(_line_num, line)| line.to_lowercase().contains(&query))
+        .map(|(line_num, line)| (line_num + 1, line))
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
     use maplit::hashmap;
 
+    // cargo test -- hello poem.txt
     #[test]
     fn test_config_new() {
+        // TODO: fix this test case
         let args = vec![
             format!("some_exe.exe"),
             format!("hello"),
             format!("poem.txt"),
         ];
-        let config = Config::new(&args).unwrap();
+        let config = Config::new(std::env::args()).unwrap();
+
         assert_eq!(config.query, args[1]);
         assert_eq!(config.filename, args[2]);
     }
 
+    // cargo test -- hello poem.txt
     #[test]
+    #[should_panic]
     fn test_config_new_insufficient_arguments() {
-        let args = vec![format!("some_exe.exe"), format!("hello")];
-        let result = Config::new(&args);
+        let result = Config::new(std::env::args());
+
         assert!(result.is_err()); // 确保返回了错误结果
         assert_eq!(result.unwrap_err(), "not enough arguments"); // 确保错误信息匹配
     }
@@ -97,7 +104,6 @@ mod tests {
             filename: String::from("poem.txt"),
             case_sensetive: false,
         };
-
         let result = run(&config);
 
         assert!(result.is_ok()); // 确保执行成功
